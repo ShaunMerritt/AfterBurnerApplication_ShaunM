d1de53b4ed937d91005310e98e9327f85fb56158
Shaun Merritt <smerrittdevelopment@gmail.com>
2014-07-11 17:27:29 -0700

    Initial Commit

---
+++ ShaunMerritt_AfterburnerApplication/SMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SMAppDelegate.h
+//  ShaunMerritt_AfterburnerApplication
+//
+//  Created by Shaun Merritt on 7/11/14.
+//  Copyright (c) 2014 True Merit Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShaunMerritt_AfterburnerApplication/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ShaunMerritt_AfterburnerApplication
+//
+//  Created by Shaun Merritt on 7/11/14.
+//  Copyright (c) 2014 True Merit Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SMAppDelegate class]));
+    }
+}
