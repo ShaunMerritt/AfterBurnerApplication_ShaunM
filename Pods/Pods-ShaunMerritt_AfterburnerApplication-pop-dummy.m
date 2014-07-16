dd5620ef7abd35561dc0bba9c27a9a69f550e236
Shaun Merritt <smerrittdevelopment@gmail.com>
2014-07-15 23:41:12 -0700

    Final

--- Pods/Pods-ShaunMerritt_AfterburnerApplication-pop-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ShaunMerritt_AfterburnerApplication_pop : NSObject
-@end
-@implementation PodsDummy_Pods_ShaunMerritt_AfterburnerApplication_pop
-@end
